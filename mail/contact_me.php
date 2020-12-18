0050f4785fc81e288efac70d6e70fbc56fd98996
lobogris26 <danielmaio62@gmail.com>
2020-12-18 10:15:55 -0300

    Primer commit

---
+++ mail/contact_me.php
@@ -0,0 +1,24 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No contiene nada!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+	
+// Create the email and send the message
+$to = 'reachingoutdesigne@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Mensaje en el sitio de: $name";
+$email_body = "Hemos recibido un nuevo mensaje a través del formulario de contacto.\n\n"."Estos son los detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nMensaje:\n$message";
+$headers = "De: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
